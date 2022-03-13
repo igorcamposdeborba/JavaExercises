aeec9c98fd532ce642c5840407b9b78b02430caa
igorcamposdeborba <52209867+igorcamposdeborba@users.noreply.github.com>
2022-03-13 17:16:34 -0300

    Update JogoDaVelha.java

--- GameTicTacToe/model/JogoDaVelha.java
+++ GameTicTacToe/model/JogoDaVelha.java
@@ -19,57 +19,34 @@ public class JogoDaVelha {
 	Scanner input = new Scanner(System.in);
 	
 	public JogoDaVelha(int dimension, Player player) {
-		createBoard(dimension);
+		createBoard(dimension); // chamar método privado criar tabuleiro
 		this.dimension = dimension;
 		this.player = player; 
 	}
 
-		// fazer jogada
-		public boolean play(int line, int column, char piece) {
-			try {
-				if (board[line][column] != 'x' && board[line][column] != 'o'){ // coloque a peça se não tiver peça naquele espaço que o usuário digitou
-					if (piece == 'x') {
-						board[line][column] = piece;
-						return true;
-					} else if (piece == 'o') {
-						board[line][column] = piece;
-						return true;
-					}
-				} else {
-					// enquanto ele digitar errado se o espaço estiver preenchido, peça para digitar certo
-					int temporaryLine = 0;
-					int temporaryColumn = 0;
-					while (board[line][column] == 'x' || board[line][column] == 'o') {
-						System.err.println("Erro: espaço já preenchido anteriormente. Digite novamente a linha"); // mostrar mensagem de erro
-						temporaryLine = input.nextInt() -1; // ! talvez corrigir com -1
-						System.out.println("Digite novamente a linha");
-						temporaryColumn = input.nextInt() -1;
-						line = temporaryLine;
-						column = temporaryColumn;
-					}
-					if (piece == 'x') {
-						board[temporaryLine][temporaryColumn] = piece;
-						return true;
-					} else if (piece == 'o') {
-						board[temporaryLine][temporaryColumn] = piece;
-						return true;
-					}
+	// fazer jogada
+	public boolean play(int line, int column, char piece) {
+		try {
+			if (board[line][column] != 'x' && board[line][column] != 'o'){ // coloque a peça se não tiver peça naquele espaço que o usuário digitou
+				if (piece == 'x') {
+					board[line][column] = piece;
+					return true;
+				} else if (piece == 'o') {
+					board[line][column] = piece;
+					return true;
 				}
-				// exception se digitou um tamanho maior que o espaço do array
-			} catch (ArrayIndexOutOfBoundsException e) {
-				System.err.println("Erro: você digitou uma linha ou coluna maior que o espaço do tabuleiro. Digite um número até " + board.length);
-				int temporaryLine = input.nextInt() -1;
-				System.out.println("Digite a coluna em que você gostaria de colocar a peça. Mínimo 1, máximo " + board.length);
-				int temporaryColumn = input.nextInt() -1;
-				
-				while (temporaryLine > board.length || temporaryColumn > board.length) {
-					System.err.println("Erro: você digitou uma linha ou coluna maior que o espaço do tabuleiro. Digite primeiro a linha, depois a coluna com números até " + board.length);
-					temporaryLine = input.nextInt() -1;
-					System.out.println("Digite a coluna em que você gostaria de colocar a peça. Mínimo 1, máximo " + board.length);
+			} else {
+				// enquanto ele digitar errado se o espaço estiver preenchido, peça para digitar certo
+				int temporaryLine = 0;
+				int temporaryColumn = 0;
+				while (board[line][column] == 'x' || board[line][column] == 'o') {
+					System.err.println("Erro: espaço já preenchido anteriormente. Digite novamente a linha"); // mostrar mensagem de erro
+					temporaryLine = input.nextInt() -1; // ! talvez corrigir com -1
+					System.out.println("Digite novamente a linha");
 					temporaryColumn = input.nextInt() -1;
-					
+					line = temporaryLine;
+					column = temporaryColumn;
 				}
-				
 				if (piece == 'x') {
 					board[temporaryLine][temporaryColumn] = piece;
 					return true;
@@ -78,6 +55,29 @@ public class JogoDaVelha {
 					return true;
 				}
 			}
+		// exception se digitou um tamanho maior que o espaço do array
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("Erro: você digitou uma linha ou coluna maior que o espaço do tabuleiro. Digite um número até " + board.length);
+			int temporaryLine = input.nextInt() -1;
+			System.out.println("Digite a coluna em que você gostaria de colocar a peça. Mínimo 1, máximo " + board.length);
+			int temporaryColumn = input.nextInt() -1;
+			
+			while (temporaryLine > board.length || temporaryColumn > board.length) {
+				System.err.println("Erro: você digitou uma linha ou coluna maior que o espaço do tabuleiro. Digite primeiro a linha, depois a coluna com números até " + board.length);
+				temporaryLine = input.nextInt() -1;
+				System.out.println("Digite a coluna em que você gostaria de colocar a peça. Mínimo 1, máximo " + board.length);
+				temporaryColumn = input.nextInt() -1;
+				
+			}
+			
+			if (piece == 'x') {
+				board[temporaryLine][temporaryColumn] = piece;
+				return true;
+			} else if (piece == 'o') {
+				board[temporaryLine][temporaryColumn] = piece;
+				return true;
+			}
+		}
 			return false;
 	}
 		
@@ -86,7 +86,12 @@ public class JogoDaVelha {
 		if (dimension > 2) {
 			this.board = new char[dimension][dimension];
 		} else {
-			System.out.println("Digite um valor a partir de 3");
+			// tratar erro: se o usuário digitou um tamanho de tabuleiro menor que 3
+			while (dimension < 3) {
+				System.out.println("Digite um valor a partir de 3");
+				dimension = input.nextInt();
+				this.board = new char[dimension][dimension];
+			}
 		}
 	}
 		
@@ -120,27 +125,23 @@ public class JogoDaVelha {
 	public boolean verifyHorizontal() {		
 		// System.out.println(Arrays.deepToString(board));  // testar visualmente se array ta preenchido
 
-		
 		int countX1 = 0; // contador para comparar se o número de peças preenchidas com x é igual ao tamanho do array da linha
 		int countO1 = 0;
 			
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) { // pergue o número do meio e no if compare com os dos do lado
 				
-				
 				// verificar horizontal
 				if (board[i][board.length - 1] == board[i][j] // pega última linha
 					&& board[i][j] == 'x') { // pega a linha atual para ver se é a peça x
-						countX1++;	 // contar o número de x daquela linha
+						countX1++; // contar o número de x daquela linha
 						if (countX1 == board.length) { // validar se x é igual ao tamanho da linha
 							player1Win = true;
 							player.setPointsPlayer1(player.getPointsPlayer1() + 1);
 							System.out.println("O jogador 1 " + player.getPlayer1() + " venceu porque preencheu a horizontal");
 							System.out.println(player.toString());
 							return true;
-						}
-						
-						
+						}	
 						
 				} else if (board[i][board.length - 1] == board[i][j] // verificar horizontal
 				    && board[i][j] == 'o') {
@@ -175,36 +176,34 @@ public class JogoDaVelha {
 				
 				// verificar vertical
 				if (board[dimension - 1][j] == board[i][j] // pegar a última linha e comparar com a atual
+					&& board[1][j] == board[i][j] // comparar a segunda linha com a atual
 					&& board[i][j] == 'x') { // pega a linha atual para comparar se é a peça x
 						countX1++;
 						
 				} else if (board[dimension - 1][j] == board[i][j]
+					&& board[1][j] == board[i][j] // comparar a segunda linha com a atual
 				    && board[i][j] == 'o') {
-						countO1++;
-							
+						countO1++;		
 				}
 			}
-			// comparar se o array da linha j está preenchido com todas as peças iguais
-			if (countX1 == board.length) { 
-				if (player1Win == false)
-					player1Win = true;
+		}
+		
+		// comparar se o array da linha j está preenchido com todas as peças iguais
+		if (countX1 == board.length) {
+			if (player1Win == false)
+				player1Win = true;
 					player.setPointsPlayer1(player.getPointsPlayer1() + 1);
 					System.out.println("O jogador 1 " + player.getPlayer1() + " venceu porque preencheu a vertical");
 					System.out.println(player.toString());
 					return true;
-			} else if (countO1 == board.length) {
-				if (player2Win == false)
-					player2Win = true;
+		} else if (countO1 == board.length) {
+			if (player2Win == false)
+				player2Win = true;
 					player.setPointsPlayer2(player.getPointsPlayer2() + 1);
 					System.out.println("O jogador 2 " + player.getPlayer2() + " venceu porque preencheu a vertical");
 					System.out.println(player.toString());
 					return true;
-			}
-			
-			countX1 = 0; // zerar contador ao trocar de linha i no array
-			countO1 = 0;
 		}
-		
 		
 		return false;
 	}
@@ -259,23 +258,32 @@ public class JogoDaVelha {
 			return false;
 		}
 	
-	public boolean verifyNoWinner() {
+	/*
+	public boolean stopLoopIfTie() {
+		if (verifyTie() == true) {
+			return true;
+		}
+		return false;
+	}*/
+	
+	// verificar empate
+	public boolean verifyTie() {
 		int countPieces = 0;
 		if (registerBoardCreated == true) { // verificar se o tabuleiro foi criado
 			for (int i = 0; i < board.length; i++) {
 				for (int j = 0; j < board[i].length; j++) {
-					if (board[i][j] == 'x' || board[i][j] == 'o') { // verificar se na posição do array ta preenchido
+					if (board[i][j] == 'x' || board[i][j] == 'o' || board[i][j] != 0) { // verificar se na posição do array ta preenchido
 						countPieces++; // contar aquela peça
 					}
 				}
 			}
-		// validar se todos os espaços estão preenchidos
-		if (countPieces == board.length) {
-			System.out.println("Ninguém venceu.");
-			System.out.println(player.toString());
+			// validar se todos os espaços estão preenchidos do tabuleiro inteiro
+			if (countPieces == board.length * dimension) { // multiplicar o tamanho do tabuleiro (ex.: 3)
+				System.out.println("Ninguém venceu.");     // para considerar não só o tamanho da linha, mas o tabuleiro completo
+				System.out.println(player.toString());
+				return true;
 			}
 		}
-		
 		return false;
 	}
 }
