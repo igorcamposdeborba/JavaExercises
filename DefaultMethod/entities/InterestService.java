f5795c17e1a6e383d3eea81b35b388938dc919ca
igorcamposdeborba <52209867+igorcamposdeborba@users.noreply.github.com>
2022-07-05 23:10:46 -0300

    Update InterestService.java

--- DefaultMethod/entities/InterestService.java
+++ DefaultMethod/entities/InterestService.java
@@ -9,6 +9,6 @@ public interface InterestService {
 	public default double payment(double amount, int months) throws InvalidParameterException { // método default não precisa implementar nas subclasses que implementam a interface
 		if (months < 1)
 			throw new InvalidParameterException();
-		return amount * Math.pow(1 + getInterestRate() / 100, months);
+		return amount * Math.pow(1.0 + getInterestRate() / 100.0, months);
 	}
 }
