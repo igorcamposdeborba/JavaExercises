d91be7c323a27be1d57ca947164f32d33b2d6bfa
igorcamposdeborba <52209867+igorcamposdeborba@users.noreply.github.com>
2022-11-02 21:40:34 -0300

    Create Comment.java

---
+++ StringBuilder/Comment.java
@@ -0,0 +1,19 @@
+package StringBuilder;
+
+public class Comment {
+	private String text;
+	
+	public Comment(String text) {
+		this.text = text;
+	}
+	
+	// Methods of access
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+}
