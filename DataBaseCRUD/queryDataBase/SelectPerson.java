ae12777015075dd1305721c23e4f7b731f557cde
igorborba <igor_campos5@hotmail.com>
2022-11-07 00:01:27 -0300

    DataBase

--- DataBaseCRUD/queryDataBase/SelectPerson.java
+++ DataBaseCRUD/queryDataBase/SelectPerson.java
@@ -6,18 +6,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import connection.ConnectionFactory;
 import entities.Person;
 
 public class SelectPerson {
 	public static void main(String[] args) throws SQLException {
-		
+		Scanner input = new Scanner(System.in);
 		Connection connection = ConnectionFactory.getConnection(); // Iniciar conexão com o banco
 		
-		String sql = "SELECT * FROM people;";
+		// String sql = "SELECT * FROM people;";
+		String sql = "SELECT * FROM people WHERE name LIKE ?;"; // sql com condição
+		
+		System.out.println("Digite um nome para buscar");
+		String search = input.nextLine();
 		
 		PreparedStatement statement = connection.prepareStatement(sql); // ! PreparedStatement protege contra SQL injection
+		statement.setString(1, "%" + search + "%"); // sql com condição e setString previne SQL injection
 		
 		ResultSet result =  statement.executeQuery(); // executar SQL E armazenar resposta recebida do banco de dados no ResultSet
 		
