84b6a1ef0dc3e913e44f016613ff6bccf5cf8f46
igorborba <igor_campos5@hotmail.com>
2023-02-27 21:33:01 -0300

    Game chess

---
+++ GameChess/src/boardgame/Board.java
@@ -0,0 +1,75 @@
+package boardgame;
+
+public class Board {
+
+	private int rows;
+	private int columns;
+	private Piece[][] pieces;
+	
+	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Erro ao criar quadro: deve haver pelo menos 1 linha e 1 coluna");
+		}
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Piece[rows][columns];
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+	
+	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Posicao fora do tabuleiro");
+		}
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao fora do tabuleiro");
+		}
+		return pieces[position.getRow()][position.getColumn()];
+	}
+	
+	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("Ja existe uma peca na posicao " + position);
+		}
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
+	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao fora do tabuleiro");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao fora do tabuleiro");
+		}
+		return piece(position) != null;
+	}
+}
---
+++ GameChess/src/chess/pieces/Rook.java
@@ -0,0 +1,55 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rook extends ChessPiece {
+
+	public Rook(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "R";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// mover para cima
+		p.setValues(position.getRow() - 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true; // marcar que peça pode andar na posição
+			p.setRow(p.getRow() - 1); // andar peça
+		}
+
+		// mover para baixo
+		p.setValues(position.getRow() + 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true; // marcar que peça pode andar na posição
+			p.setRow(p.getRow() + 1); // andar peça
+		}
+
+		// mover para esquerda
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true; // marcar que peça pode andar na posição
+			p.setColumn(p.getColumn() - 1); // andar peça
+		}
+
+		// mover para direita
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true; // marcar que peça pode andar na posição
+			p.setColumn(p.getColumn() + 1); // andar peça
+		}
+		
+		return mat;
+	}
+}
