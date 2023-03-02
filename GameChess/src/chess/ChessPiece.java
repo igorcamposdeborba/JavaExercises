17d26775f1996d6bfcc481ebc29315c182746ae0
igorborba <igor_campos5@hotmail.com>
2023-03-01 21:30:52 -0300

    Chess game

--- GameChess/src/chess/ChessPiece.java
+++ GameChess/src/chess/ChessPiece.java
@@ -7,7 +7,8 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
 	private Color color;
-
+	private int moveCount;
+	
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
@@ -15,6 +16,9 @@ public abstract class ChessPiece extends Piece {
 
 	public Color getColor() {
 		return color;
+	}
+	public int getMoveCount() {
+		return moveCount;
 	}
 	
 	protected boolean isThereOpponentPiece(Position position) {
@@ -25,5 +29,12 @@ public abstract class ChessPiece extends Piece {
 	
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
+	}
+	
+	protected void increaseMoveCount() {
+		moveCount++;
+	}
+	protected void decreaseMoveCount() {
+		moveCount--;
 	}
 }
---
+++ GameChess/src/chess/pieces/Pawn.java
@@ -0,0 +1,79 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Pawn extends ChessPiece {
+	public Pawn(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		if (getColor() == Color.WHITE) {
+			// Mover 1 casa para cima
+			p.setValues(position.getRow() - 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+
+			// Mover 2 casas para cima na primeira jogada
+			p.setValues(position.getRow() - 2, position.getColumn());
+			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getMoveCount() == 0
+					&& getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+
+			// Mover para capturar peça na diagonal esquerda
+			p.setValues(position.getRow() - 1, position.getColumn() - 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+
+			// Mover para capturar peça na diagonal direita
+			p.setValues(position.getRow() - 1, position.getColumn() + 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+		} else {
+			// Mover 1 casa para cima
+			p.setValues(position.getRow() + 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+
+			// Mover 2 casas para cima na primeira jogada
+			p.setValues(position.getRow() + 2, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getMoveCount() == 0
+					&& getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+
+			// Mover para capturar peça na diagonal esquerda
+			p.setValues(position.getRow() + 1, position.getColumn() - 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+
+			// Mover para capturar peça na diagonal direita
+			p.setValues(position.getRow() + 1, position.getColumn() + 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+		}
+		return mat;
+	}
+	
+	@Override
+	public String toString() {
+		return "P";
+	}
+}
