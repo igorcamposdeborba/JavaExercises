5aeb4a116ac2f8f71e1ad09d700be5f0c0215b81
igorborba <igor_campos5@hotmail.com>
2023-03-01 22:45:14 -0300

    Chess game

--- GameChess/src/chess/pieces/Queen.java
+++ GameChess/src/chess/pieces/Queen.java
@@ -5,15 +5,10 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece {
+public class Queen extends ChessPiece {
 
-	public Rook(Board board, Color color) {
+	public Queen(Board board, Color color) {
 		super(board, color);
-	}
-
-	@Override
-	public String toString() {
-		return "R";
 	}
 
 	@Override
@@ -61,7 +56,52 @@ public class Rook extends ChessPiece {
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
+
+		// mover para noroeste
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true; // marcar que peça pode andar na posição
+			p.setValues(p.getRow() - 1, p.getColumn() - 1); // andar peça
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// mover para nordeste
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true; // marcar que peça pode andar na posição
+			p.setValues(p.getRow() - 1, p.getColumn() + 1); // andar peça
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// mover para sudeste
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true; // marcar que peça pode andar na posição
+			p.setValues(p.getRow() + 1, p.getColumn() + 1); // andar peça
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// mover para sudoeste
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true; // marcar que peça pode andar na posição
+			p.setValues(p.getRow() + 1, p.getColumn() - 1); // andar peça
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
 		return mat;
+	}
+
+	@Override
+	public String toString() {
+		return "Q";
 	}
 }
