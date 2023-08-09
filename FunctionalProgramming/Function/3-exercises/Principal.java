215b741f29a827111e08f1b11309b15b6b781eb0
igorcamposdeborba <igor_campos5@hotmail.com>
2023-08-08 22:29:21 -0300

    Function smallest element

---
+++ FunctionalProgramming/Function/3-exercises/Principal.java
@@ -0,0 +1,38 @@
+package principal;
+
+import java.util.Optional;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class Principal {
+	public static void main (String [] args) {
+		
+		// 1) menorDeDois: recebe dois valores e retorna o menor
+		double a = 12.1;
+	    double b = 13.0;
+	    
+	    
+	// Opção 1:
+		Function<Double, Double> compareElements = x -> x >= b == true ? b : x;
+		
+		Optional<Double> smallestElement = Stream.of(a)
+													.map(compareElements)
+													.findFirst();
+		
+		smallestElement.ifPresent(System.out::println);
+		
+	// Opção 2:
+		Double comparedElements = Stream.of(a, b)
+											// .reduce(Double::min)
+											.reduce((x, y) -> x >= y == true ? y : x)
+											.orElse(Double.NaN);
+		System.out.println(comparedElements);
+		
+	// Opção 3:
+		Double compare = Stream.of(a, b)
+									.reduce(Double::min) // reference method
+									.get();
+		System.out.println(compare);
+	}
+}
